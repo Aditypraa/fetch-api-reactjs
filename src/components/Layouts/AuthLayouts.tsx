1a03388c166d476f5d3f2a9d3fb560eae31b29bc
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-22 05:17:41 +0700

    refactor : implemen "conditional rendering" in file AuthLayouts.tsx

--- src/components/Layouts/AuthLayouts.tsx
+++ src/components/Layouts/AuthLayouts.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 function AuthLayouts({ title, description, children }: { title: string, description: string, children: React.ReactNode }) {
     return (
@@ -6,6 +7,15 @@ function AuthLayouts({ title, description, children }: { title: string, descript
                 <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
                 <p className="font-medium text-slate-500 mb-8">{description}</p>
                 {children}
+
+                <p className="text-sm mt-5 text-center">
+                    {/* Penerapan Conditional Rendering */}
+                    {title === "Login" ? "Belum Mempunyai Akun ? " : "Sudah Mempunyai Akun ? "}
+
+                    {/* Conditional Rendeting Button Login dan Register */}
+                    {title === "Login" && <Link to="/register" className="font-bold text-blue-600"> Sign up</Link>}
+                    {title === "Register" && <Link to="/" className="font-bold text-blue-600"> Login</Link>}
+                </p>
             </div>
         </div>
     )
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import FormLogin from "../components/Fragments/FormLogin";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 
@@ -6,10 +5,6 @@ function LoginPage() {
     return (
         <AuthLayouts title="Login" description="Selamat Datang dan Silakan Login">
             <FormLogin />
-            <p className="text-sm mt-5 text-center">
-                Belum Mempunyai Akun ?
-                <Link to="/register" className="font-bold text-blue-600"> Sign up</Link>
-            </p>
         </AuthLayouts>
     )
 }
