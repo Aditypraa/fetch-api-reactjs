5adde06164c575c08d0e7571f6f0222c6e02027d
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-22 04:52:20 +0700

    feat : implemen react-router-dom

--- src/components/Layouts/AuthLayouts.tsx
+++ src/components/Layouts/AuthLayouts.tsx
@@ -1,10 +1,12 @@
 
 function AuthLayouts({ title, description, children }: { title: string, description: string, children: React.ReactNode }) {
     return (
-        <div className="w-full max-w-xs">
-            <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
-            <p className="font-medium text-slate-500 mb-8">{description}</p>
-            {children}
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="w-full max-w-xs">
+                <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
+                <p className="font-medium text-slate-500 mb-8">{description}</p>
+                {children}
+            </div>
         </div>
     )
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "../pages/login";
+import RegisterPage from "../pages/register";
+import ErrorPage from "../pages/errorPage";
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<LoginPage />} errorElement={<ErrorPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+    )
+}
