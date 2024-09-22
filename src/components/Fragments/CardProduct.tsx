7eebefaf76bd32307bfdb4e0b8d5f4ba393e13a2
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-23 00:28:09 +0700

    ✅ "USE STATE"

--- src/components/Fragments/CardProduct.tsx
+++ src/components/Fragments/CardProduct.tsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button"
 
 function CardProduct({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col justify-between w-full max-w-sm bg-gray-800 border border-gray-200 rounded-lg shadow mx-2">
+        <div className="flex flex-col justify-between w-full max-w-xs bg-gray-800 border border-gray-200 rounded-lg shadow mx-2 my-2">
             {children}
         </div>
     )
@@ -29,11 +29,11 @@ const Content = ({ title, children }: { title: string, children: React.ReactNode
     )
 }
 
-const Footer = ({ price }: { price: string }) => {
+const Footer = ({ price, id, handleAddToCard }: { price: number, id: number, handleAddToCard: (id: number) => void }) => {
     return (
         <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">Rp. {price}</span>
-            <Button type="button" className="bg-blue-600">Add To Card</Button>
+            <span className="text-xl font-bold text-white">{price.toLocaleString("id-ID", { style: 'currency', currency: 'IDR' })}</span>
+            <Button type="button" className="bg-blue-600" onClick={() => handleAddToCard(id)}>Add To Card</Button>
         </div>
     )
 }
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Button from "../components/Elements/Button"
 import CardProduct from "../components/Fragments/CardProduct"
 
@@ -5,28 +6,28 @@ const dataProducts = [
     {
         id: 1,
         title: "Laptop",
-        price: "6.000.000",
+        price: 6000000,
         images: "images/laptop.avif",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente adipisci eius quod qui ea sunt placeat tempora obcaecati itaque perspiciatis. Obcaecati, nostrum ipsam. Labore minus dignissimos harum veritatis ducimus distinctio!"
     },
     {
         id: 2,
         title: "Smartphone",
-        price: "4.000.000",
+        price: 4000000,
         images: "images/laptop.avif",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente adipisci eius quod qui ea sunt placeat tempora obcaecati itaque perspiciatis. Obcaecati, nostrum ipsam. Labore minus dignissimos harum veritatis ducimus distinctio!"
     },
     {
         id: 3,
         title: "Headset",
-        price: "1.000.000",
+        price: 1000000,
         images: "images/laptop.avif",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente adipisci eius quod qui ea sunt placeat tempora obcaecati itaque perspiciatis. Obcaecati, nostrum ipsam. Labore minus dignissimos harum veritatis ducimus distinctio!"
     },
     {
         id: 4,
         title: "Keyboard",
-        price: "500.000",
+        price: 500000,
         images: "images/laptop.avif",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente adipisci eius quod qui ea sunt placeat tempora obcaecati itaque perspiciatis. Obcaecati, nostrum ipsam. Labore minus dignissimos harum veritatis ducimus distinctio!"
     }
@@ -36,6 +37,12 @@ const dataProducts = [
 const email = localStorage.getItem("email")
 
 function ProductsPage() {
+    const [card, setCard] = useState([
+        {
+            id: 1,
+            qty: 1
+        }
+    ])
 
     // Implement event handler for logout button
     const handleLogout = () => {
@@ -44,22 +51,61 @@ function ProductsPage() {
         window.location.href = "/"
     }
 
+    const handleAddToCard = (id: number) => {
+        if (card.find((item) => item.id === id)) { // cek apakah item sudah ada di card
+            setCard(card.map((item) => item.id === id ? { ...item, qty: item.qty + 1 } : item)) // jika sudah ada, tambahkan qty
+        } else {
+            setCard([...card, { id, qty: 1 }]) // jika belum ada, tambahkan item ke card
+        }
+    }
+
     return (
         <>
             <div className="flex justify-end items-center h-20 bg-violet-500 text-white px-10">
                 {email}
                 <Button type="button" className="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
             </div>
+
             <div className="flex justify-center py-5">
-                {dataProducts.map((product) => (
-                    <CardProduct key={product.id}>
-                        <CardProduct.Header images={product.images} />
-                        <CardProduct.Content title={product.title}>
-                            {product.description}
-                        </CardProduct.Content>
-                        <CardProduct.Footer price={product.price} />
-                    </CardProduct>
-                ))}
+                <div className="flex flex-wrap w-4/6">
+                    {dataProducts.map((product) => (
+                        <CardProduct key={product.id}>
+                            <CardProduct.Header images={product.images} />
+                            <CardProduct.Content title={product.title}>
+                                {product.description}
+                            </CardProduct.Content>
+                            <CardProduct.Footer price={product.price} id={product.id} handleAddToCard={handleAddToCard} />
+                        </CardProduct>
+                    ))}
+                </div>
+
+                {/* Card */}
+                <div className="w-2/6">
+                    <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Card</h1>
+                    <table className="text-left table-auto border-separate border-spacing-x-5">
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {card.map((item) => {
+                                const product = dataProducts.find((product) => product.id === item.id)
+                                return product ? (
+                                    <tr key={item.id}>
+                                        <td>{product.title}</td>
+                                        <td>{product.price.toLocaleString("id-ID", { style: 'currency', currency: 'IDR' })}</td>
+                                        <td>{item.qty}</td>
+                                        <td>{(item.qty * product.price).toLocaleString("id-ID", { style: 'currency', currency: 'IDR' })}</td>
+                                    </tr>
+                                ) : null
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
